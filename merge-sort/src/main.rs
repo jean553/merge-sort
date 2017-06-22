fdade5836790e5e68a6228140969e616efb7cec9
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-06-22 23:32:03 +0200

    collect user input

--- merge-sort/src/main.rs
+++ merge-sort/src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<_> = env::args().collect();
+    let mut sub_array: Vec<u8> = Vec::new();
+
+    for arg in args {
+
+        let input = arg.trim().parse::<u8>();
+
+        /* ignores additional input before digits */
+        if input.is_err() {
+            continue;
+        }
+
+        sub_array.push(input.unwrap());
+    }
 }
