e08e5e5965819368fc906f4d7fdc6118e9618f0a
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-06-26 20:57:01 +0200

    integrate basic test for two items array

--- merge-sort/src/lib.rs
+++ merge-sort/src/lib.rs
@@ -1,6 +1,34 @@
+mod ms {
+
+    pub fn merge_sort(
+        source: &mut [u8],
+        destination: &mut [u8],
+        start: usize,
+        end: usize,
+    ) {
+    }
+}
+
 #[cfg(test)]
 mod tests {
+
     #[test]
-    fn it_works() {
+    fn test_with_two_items_array() {
+
+        let source = [7, 5];
+        let destination = [0, 0];
+
+        merge_sort(
+            &mut source,
+            &mut destination,
+            0,
+            2,
+        );
+
+        assert_eq!(
+            destination,
+            [5, 7],
+            "two items array cannot be sorted",
+        );
     }
 }
